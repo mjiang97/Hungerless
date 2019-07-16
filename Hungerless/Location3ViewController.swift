63dc1b1a93a81f5581c54d6ed381175478a213bc
Girls Code <GWC@cs81-ms-crt-07.local>
2019-07-16 15:22:41 -0400

    added new view controller file

--- Hungerless/Location3ViewController.swift
+++ Hungerless/Location3ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class Location3ViewController: UIViewController {
 
-    override func viewDidLoad() {
+  /*  override func viewDidLoad() {
         super.viewDidLoad()
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
@@ -21,7 +21,7 @@ class Location3ViewController: UIViewController {
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
-        marker.map = mapView
+        marker.map = mapView */
 
         // Do any additional setup after loading the view.
     }
